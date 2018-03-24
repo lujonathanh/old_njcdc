e99a961a4cbc0f6972bf0a9e30f1a35e903e6c7f
dcw3 <dcw3@princeton.edu>
2018-03-23 21:45:51 -0400

    stuff

--- njcdc/settings.py
+++ njcdc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,6 +73,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'njcdc.wsgi.application'
 
+
+# DATABASES
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'njcdc',
+        'USER': 'dcw3',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
