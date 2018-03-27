1a09af7b2ba0d1dd251c4ae3c0e8e09c519fdb85
lujonathanh <lujonathanh@gmail.com>
2018-03-27 17:09:22 -0400

    Updated Views-- still failing

--- calc/views.py
+++ calc/views.py
@@ -3,19 +3,28 @@ from django.template import loader
 from django.contrib import messages
 from django.urls import reverse
 from .forms import InputForm
+from .models import UserProfile
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the calc index.")
 
 def input(request):
+    #http://127.0.0.1:8000/print("INputting")
     if request.method == 'POST':
         input_form = InputForm(request.POST)
 
         if input_form.is_valid():
             user = input_form.save()
             messages.success(request, "Thank you for entering")
-            return HttpResponseRedirect(reverse(results))
+            #return HttpResponseRedirect(reverse('input'))
+            #return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect(reverse('results', args=(user.id,)))
+            # return HttpResponseRedirect(reverse('results', kwargs={'id' : input_form.instance.id}))
+
+            # return HttpResponseRedirect(reverse('results', args=[input_form.instance.id]))
+            # return HttpResponseRedirect(reverse('results', kwargs={'id': user.id}))
+            # return HttpResponseRedirect(reverse('results', kwargs={'up': user}))
         messages.error(request, 'There were errors. Please try again.')
     else:
         input_form = InputForm()
@@ -24,11 +33,13 @@ def input(request):
     context = {'input_form': input_form, 'formtitle': "Input"}
     return HttpResponse(template.render(context, request))
 
-def results(request):
+def results(request, id):
     # response = "You're looking at the results of entering with profile %s."
-    up = request.UserProfile
+    print("Arrived")
+
+    up = UserProfile.objects.get(id=id)
     up.calculate_net()
 
     template = loader.get_template('calc/results.html')
-    context = {'up' : request.UserProfile}
+    context = {'up' : up}
     return HttpResponse(template.render(context, request))
